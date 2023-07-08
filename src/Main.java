bcc7f622751951c61af1e03479b9686109810e89
ArtemZaitsev93 <aazaitsev93@gmail.com>
2023-07-08 13:14:58 -0400

    The feture Sun has benn added succesfully

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         Omnax.Planets="2";
         Omnax.star="4";
 
+        FeatureSun Xein = new FeatureSun();
+        Xein.name="treo";
+        Xein.size="328384km";
+        Xein.temp="-123131313 F";
+        
+
     }
 }
